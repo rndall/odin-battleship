f28ef63c2b66fc0bdc1c6e3753339265081e2140
Ran <96964503+rndall@users.noreply.github.com>
2025-05-21 18:26:15 +0800

    test(gameboard): add board property test

---
+++ tests/Gameboard.test.js
@@ -0,0 +1,19 @@
+const Gameboard = require("../classes/Gameboard")
+
+describe("gameboard", () => {
+  const gameboard = new Gameboard()
+
+  test("board", () =>
+    expect(gameboard.board).toEqual([
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+    ]))
+})
