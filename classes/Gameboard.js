a3953329fbdde93ee1537b60e127fc962eebb48c
Ran <96964503+rndall@users.noreply.github.com>
2025-05-21 23:03:07 +0800

    feat(gameboard): add placeShip punction and implement vertical logic

--- classes/Gameboard.js
+++ classes/Gameboard.js
@@ -2,6 +2,17 @@ class Gameboard {
   constructor() {
     this.board = Array(10).fill(Array(10).fill(null))
   }
+
+  placeShip(ship, start, orientation) {
+    const [y, x] = start
+    const endSquare = y + ship.length
+
+    if (orientation === "vertical") {
+      for (let i = y; i < endSquare; i++) {
+        this.board[i][x] = ship
+      }
+    }
+  }
 }
 
 module.exports = Gameboard
