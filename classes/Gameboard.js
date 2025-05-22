6ec23e22223ab1c22661d1d9d52942efd23c3b45
Ran <96964503+rndall@users.noreply.github.com>
2025-05-22 16:31:47 +0800

    fix(gameboard): prevent ships from being placed outside the board

--- classes/Gameboard.js
+++ classes/Gameboard.js
@@ -8,14 +8,18 @@ class Gameboard {
 
   placeShip(ship, x, y, orientation) {
     let endSquare = ship.length
+    const boardSize = 10
+    const err = new Error("Cannot place ship outside the grid boundaries.")
 
     if (orientation === "vertical") {
       endSquare += y
+      if (endSquare > boardSize) throw err
       for (let i = y; i < endSquare; i++) {
         this.board[i][x] = ship
       }
     } else if (orientation === "horizontal") {
       endSquare += x
+      if (endSquare > boardSize) throw err
       for (let i = x; i < endSquare; i++) {
         this.board[y][i] = ship
       }
