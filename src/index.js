42554e2a5937e9f0fc1c68dedbcf08ddeaae8f87
Ran <96964503+rndall@users.noreply.github.com>
2025-05-25 17:05:51 +0800

    feat: setup new game and prepopulate boards

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import "./css/reset.css"
 import "./css/style.css"
+import { populateBoards } from "./modules/game-controller"
+
+populateBoards()
---
+++ src/modules/game-controller.js
@@ -0,0 +1,32 @@
+import Player from "../classes/Player"
+import Ship from "../classes/Ship"
+
+const realPlayer = new Player("real")
+const computerPlayer = new Player("computer")
+
+const activePlayer = realPlayer
+
+const placeShips = (player) => {
+  const shipLengths = [2, 3, 3, 4, 5]
+  const coordinates = [
+    { x: 0, y: 0, orientation: "horizontal" },
+    { x: 0, y: 1, orientation: "horizontal" },
+    { x: 0, y: 2, orientation: "horizontal" },
+    { x: 0, y: 3, orientation: "horizontal" },
+    { x: 0, y: 4, orientation: "horizontal" },
+  ]
+
+  for (let i = 0; i < 5; i++) {
+    const { x, y, orientation } = coordinates[i]
+
+    const ship = new Ship(shipLengths[i])
+    player.board.placeShip(ship, x, y, orientation)
+  }
+}
+
+const populateBoards = () => {
+  placeShips(realPlayer)
+  placeShips(computerPlayer)
+}
+
+export { activePlayer, populateBoards }
