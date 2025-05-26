53e17a79948a71ee3867fbddf82eb50930e17c27
Ran <96964503+rndall@users.noreply.github.com>
2025-05-26 12:40:47 +0800

    fix: computer attack

--- src/modules/game-controller.js
+++ src/modules/game-controller.js
@@ -53,8 +53,8 @@ const playerAttack = (x, y, target) => {
 }
 
 const getRandomCoordinates = () => {
-  const x = Math.floor(Math.random() * 9)
-  const y = Math.floor(Math.random() * 9)
+  const x = Math.floor(Math.random() * 10)
+  const y = Math.floor(Math.random() * 10)
 
   return [x, y]
 }
